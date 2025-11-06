ab1f92f8387eed94f91bb9b507e1e7a358b7f4c6
Eric Garner <eric.adam.garner@gmail.com>
2025-11-06 16:26:32 -0500

    feat: add test_makefile

--- tests/test_generate_project.py
+++ tests/test_generate_project.py
@@ -1,13 +1,34 @@
+from re import sub
 import subprocess
 from pathlib import Path
+import json
+
 
 THIS_DIR = Path(__file__).parent
 PROJECT_DIR = THIS_DIR.parent
 
 def test_generate_project():
-    subprocess.run([
+    
+    cookie_cutter_config = {
+        "default_context": {"repo_name": "test-repo"}
+    }
+    
+    cookie_cutter_config_path = PROJECT_DIR / "cookie-cutter-test-config.json"
+    cookie_cutter_config_path.write_text(json.dumps(cookie_cutter_config))
+    
+    cmd = [
         "cookiecutter",
         str(PROJECT_DIR),
         "--output-dir",
         str(PROJECT_DIR / "sample"),
-        ], check=True)
+        "--no-input",
+        "--config-file",
+        str(cookie_cutter_config_path)
+        ]
+    
+    subprocess.run(cmd, check=True)
+    
+    generated_repo_dir = PROJECT_DIR / "sample" / cookie_cutter_config["default_context"]["repo_name"]
+    assert generated_repo_dir.exists()
+    
+    subprocess.run(["make", "clean"])
